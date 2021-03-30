71efc6859b429bd67f5b5be3a8817a44fb527928
Jason <jfelton8899@gmail.com>
2021-03-30 11:55:49 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,123 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // card options
+const cardArray = [
+    {
+        name: 'wavy-lines',
+        img: 'images/black-and-white-wavy-lines.png'
+    },
+    {
+        name: 'wavy-lines',
+        img: 'images/black-and-white-wavy-lines.png'
+    },
+    {
+        name: 'bluegreen-check',
+        img: 'images/bluegreen-checkerboard.png'
+    },
+    {
+        name: 'bluegreen-check',
+        img: 'images/bluegreen-checkerboard.png'
+    },
+    {
+        name: 'inverted-nine',
+        img: 'images/inverted-nine-domino.png'
+    },
+    {
+        name: 'inverted-nine',
+        img: 'images/inverted-nine-domino.png'
+    },
+    {
+        name: 'nine-domino',
+        img: 'images/nine-domino.png'
+    },
+    {
+        name: 'nine-domino',
+        img: 'images/nine-domino.png'
+    },
+    {
+        name: 'inv-yellow',
+        img: 'images/inv-yellow-black-diagonal.png'
+    },
+    {
+        name: 'inv-yellow',
+        img: 'images/inv-yellow-black-diagonal.png'
+    },
+    {
+        name: 'red-polka',
+        img: 'images/red-polka-on-white.png'
+    },
+    {
+        name: 'red-polka',
+        img: 'images/red-polka-on-white.png'
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random());
+
+const grid = document.querySelector('.grid');
+const resultDisplay = document.querySelector('#result');
+const alertDisplay = document.querySelector('#alerts');
+let cardsChosen = [];
+let cardsChosenId = [];
+let cardsWon = [];
+
+// create gameboard
+function createBoard() {
+    for(let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img');
+        card.setAttribute('src', 'images/card-back.png');
+        card.setAttribute('data-id', i);
+        card.addEventListener('click', flipcard);
+        grid.appendChild(card);
+    }
+}
+
+// check for matches
+function checkForMatch() {
+    const cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+
+    if (optionOneId === optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'images/card-back.png');
+        cards[optionTwoId].setAttribute('src', 'images/card-back.png');
+        alertDisplay.textContent = 'You have clicked the same image!';
+    }
+    else if (cardsChosen[0] === cardsChosen[1]) {
+        alertDisplay.textContent = 'You found a match';
+        cards[optionOneId].setAttribute('src', 'images/plain-white.png');
+        cards[optionTwoId].setAttribute('src', 'images/plain-white.png');
+        cards[optionOneId].removeEventListener('click', flipcard);
+        cards[optionTwoId].removeEventListener('click', flipcard);
+        cardsWon.push(cardsChosen);
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/card-back.png');
+        cards[optionTwoId].setAttribute('src', 'images/card-back.png');
+        alertDisplay.textContent = 'Sorry, try again';
+    }
+
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = `Matches Made: ${cardsWon.length}`;
+    if (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulations! You found them all!';
+    }
+}
+
+
+// flip your card
+function flipcard() {
+    let cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500);
+    }
+}
+
+createBoard();
+})
+
+// credit to Ania Kobow for the YouTube tutorial on the code for this game. I designed all the cards and card backs myself in Adobe Illustrator and changed the styling of the webpage to make it my own.
+
+// I also changed from using pop-up alerts to using textContent to display the messages directly on the page
